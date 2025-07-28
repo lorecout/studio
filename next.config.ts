8cc647b5a4a3690080cf2ed1947c8756d2fcfc7c
Lorena Coutinho <lorenalessa90@gmail.com>
2025-07-28 21:55:21 +0000

    ⚠ Blocked cross-origin request from 6000-firebase-studio-1753729608951.c

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  allowedDevOrigins: ["https://6000-firebase-studio-1753729608951.cluster-duylic2g3fbzerqpzxxbw6helm.cloudworkstations.dev"],
 };
 
 export default nextConfig;
