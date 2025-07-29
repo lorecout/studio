5e2f88b0c8d6b8677eb8a5917c1d6b4f44ee197e
Lorena Coutinho <lorenalessa90@gmail.com>
2025-07-29 13:46:44 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,12 +7,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  "projectId": "realgoal",
+  "appId": "1:10684111974:web:b88ea9dbf41209980861a7",
+  "storageBucket": "realgoal.firebasestorage.app",
+  "apiKey": "AIzaSyAXaJKtFL5l3jNoiL2_nNlgqYj0k-HTF7c",
+  "authDomain": "realgoal.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "10684111974"
 };
 
 // Initialize Firebase
